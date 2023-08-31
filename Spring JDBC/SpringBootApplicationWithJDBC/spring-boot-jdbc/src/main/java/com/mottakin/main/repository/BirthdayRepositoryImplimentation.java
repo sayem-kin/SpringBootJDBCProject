8fba582ec3e485fecfdb2dcd27593d67a9bf0f21
sayem-kin <85879297+sayem-kin@users.noreply.github.com>
2023-08-31 22:56:01 +0600

    Add files via upload

---
+++ Spring JDBC/SpringBootApplicationWithJDBC/spring-boot-jdbc/src/main/java/com/mottakin/main/repository/BirthdayRepositoryImplimentation.java
@@ -0,0 +1,55 @@
+package com.mottakin.main.repository;
+
+import com.mottakin.main.model.Birthday;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class BirthdayRepositoryImplimentation implements IBirthdayRepository{
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public Optional<Birthday> getById(Integer id) {
+        String query = "select * from birthday_table where id = ?";
+        return jdbcTemplate.queryForObject(query, new Object[]{id}, (resultSet, rowNum) ->
+                Optional.of(
+                        new Birthday(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("date_inserted")
+                        )
+                )
+        );
+    }
+
+    @Override
+    public List<Birthday> getAll() {
+        String query = "select * from birthday_table";
+        return jdbcTemplate
+                .query(query, (resultSet, rowNum) ->
+                        new Birthday(
+                                resultSet.getInt("id"),
+                                resultSet.getString("name"),
+                                resultSet.getString("date_inserted")
+                        )
+                );
+    }
+    @Override
+    public void deleteBirthday(Integer id) {
+        String query = "delete from birthday_table where id = ?";
+        jdbcTemplate.update(query, id);
+    }
+
+    @Override
+    public void add(Birthday birthday) {
+        String query = "insert into birthday_table (id, name, date_inserted) values (?, ?, ?)";
+        jdbcTemplate.update(query, birthday.getId(), birthday.getName(), birthday.getDate_inserted());
+
+    }
+}
---
+++ Spring JDBC/SpringBootApplicationWithJDBC/spring-boot-jdbc/src/main/java/com/mottakin/main/repository/IBirthdayRepository.java
@@ -0,0 +1,13 @@
+package com.mottakin.main.repository;
+
+import com.mottakin.main.model.Birthday;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IBirthdayRepository {
+    Optional<Birthday> getById(Integer id);
+    List<Birthday> getAll();
+    void add(Birthday birthday);
+    void deleteBirthday(Integer id);
+}
